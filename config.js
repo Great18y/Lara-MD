4da01a582468cb161146340f6fcdc2a61de8ec0f
Great18y <167424812+Great18y@users.noreply.github.com>
2024-12-21 23:36:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "yiRjBLJD#WYsigvg58rAjVwwXBlgefAY-OTLvkxWzniMWOCYuZGg", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "OjZCDY7L#59Mp_PfjpPVBM2xwsmuo5aQjVragjvjnRj4zJx3WZe4", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
